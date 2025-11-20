2919508ed06f050e31abeda0fb0272f2042bc19c
Ravi Kumar Patel <ravikumarpatel2001@gmail.com>
2025-11-20 14:54:51 +0530

    20-11-2025 Day-Thursday Coding Problems

--- 20_11_25_user_info.c
+++ 20_11_25_user_info.c
@@ -3,17 +3,17 @@
 int main() {
     int age;
     float marks;
-    char grade; //
-    printf("Enter your grade: "); 
-    scanf("%c", &grade);
-
+    char grade;
+    
     printf("Enter your age: ");
     scanf("%d", &age);
 
     printf("Enter your marks: ");
     scanf("%f", &marks);
 
+    printf("Enter your grade: "); 
+    scanf(" %c", &grade);
+
     printf("Age = %d, Marks = %.2f, Grade = %c", age, marks, grade);
-    // printf("Age = %d, Marks = %.2f", age, marks);
     return 0;
 }
